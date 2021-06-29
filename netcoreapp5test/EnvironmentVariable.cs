e890da48821d76de14ddb3869459316560ec6a6d
Rajkumar Rangaraj <rajrang@microsoft.com>
2021-06-29 15:35:44 -0700

    Add project files.

---
+++ netcoreapp5test/EnvironmentVariable.cs
@@ -0,0 +1,29 @@
+﻿namespace Shared
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class EnvironmentVariable
+    {
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+
+        public static IEnumerable<EnvironmentVariable> GetEnvironmentVariables()
+        {
+            List<EnvironmentVariable> envVariables = new List<EnvironmentVariable>();
+            foreach (DictionaryEntry env in Environment.GetEnvironmentVariables())
+            {
+                string key = (string)env.Key;
+                string value = (string)env.Value;
+                EnvironmentVariable envVariable = new EnvironmentVariable { Name = key, Value = value };
+                envVariables.Add(envVariable);
+            }
+            envVariables.Sort((x, y) => x.Name.CompareTo(y.Name));
+
+            return envVariables.ToArray();
+        }
+    }
+}
---
+++ netcoreapp5test/LoadedDLL.cs
@@ -0,0 +1,28 @@
+﻿namespace Shared
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System.Text;
+    public class LoadedDLL
+    {
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+
+        public static IEnumerable<LoadedDLL> GetLoadedDLLs()
+        {
+            List<LoadedDLL> loadedDLLs = new List<LoadedDLL>();
+            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                string assemblyName = assembly.GetName().ToString();
+                string assemblyLocation = assembly.IsDynamic ? "" : assembly.Location;
+                LoadedDLL dll = new LoadedDLL { Name = assemblyName, Location = assemblyLocation };
+                loadedDLLs.Add(dll);
+            }
+            loadedDLLs.Sort((x, y) => x.Name.CompareTo(y.Name));
+
+            return loadedDLLs.ToArray();
+        }
+    }
+}
